b9d1d6545bf53f6c9d02951a5a0900e09ca78788
sivakishores1994 <108855254+sivakishores1994@users.noreply.github.com>
2024-03-20 23:34:27 +0530

    Update findDuplicatesFromArray.js

--- src/JS Propblem Solving/findDuplicatesFromArray.js
+++ src/JS Propblem Solving/findDuplicatesFromArray.js
@@ -1,1 +1,20 @@
-cons
+//find dupicates from array and display the duplicates with duplicate count
+
+let arr=['hi','hello','hi','siva','siva'];
+let unique=[];
+let duplicates=[];
+let count=0;
+arr.forEach(num =>{
+    if(unique.indexOf(num)==-1){
+        unique.push(num);
+    }
+    else{
+             duplicates.push(num)
+    }
+});
+
+const newObj=duplicates.map(num => [num, arr.filter(validation => num===validation).length])
+console.log(newObj)
+
+//op: 
+// [ [ 'hi', 2 ], [ 'siva', 2 ] ]
