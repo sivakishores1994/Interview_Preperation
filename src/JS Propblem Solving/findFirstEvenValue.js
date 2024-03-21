43edd2ad16f454102747bdacec66ba8c391fa610
sivakishores1994 <108855254+sivakishores1994@users.noreply.github.com>
2024-03-21 10:22:20 +0530

    Update findFirstEvenValue.js

--- src/JS Propblem Solving/findFirstEvenValue.js
+++ src/JS Propblem Solving/findFirstEvenValue.js
@@ -3,3 +3,15 @@ const numbers=[1,3,4,5,2,7,6];
 const firstEven=numbers.find(num=> num%2===0);
 
 console.log(numbers)
+
+//using loop
+
+const arr=[22,10,77,77];
+let temp=0;
+for(let i=0;i<arr.length;i++) {
+    if(arr[i]%2===0){
+        temp=arr[i];
+        break;
+    }
+}
+console.log(temp)
