c664a92b368c2276750274a29ebb1ecbfb1354e8
sivakishores1994 <108855254+sivakishores1994@users.noreply.github.com>
2024-03-21 09:09:06 +0530

    Update findDuplicatesFromArray.js

--- src/JS Propblem Solving/findDuplicatesFromArray.js
+++ src/JS Propblem Solving/findDuplicatesFromArray.js
@@ -14,7 +14,20 @@ arr.forEach(num =>{
 });
 
 const newObj=duplicates.map(num => [num, arr.filter(validation => num===validation).length])
-console.log(newObj)
+console.log(newObj.filter(num=> num !==1)
 
 //op: 
 // [ [ 'hi', 2 ], [ 'siva', 2 ] ]
+
+//using map and filter
+
+const arr1=[1,2,3,2,4,3,1,1,2];
+const duplicates=[];
+let count=0
+const filteredVas=arr1.filter((val,index,arrs)=>{
+    return arrs.indexOf(val)===index;
+})
+const duplWithCounts=filteredVas.map((num)=> [num, arr1.filter((val)=> val===num).length])
+console.log(duplWithCounts.filter((num)=>num[1]!==1))
+
+//op: [ [ 1, 3 ], [ 2, 3 ], [ 3, 2 ] ]
