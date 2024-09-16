1f8b5066bdfb624c7f13ffc514be15ae5666871a
sivakishores1994 <108855254+sivakishores1994@users.noreply.github.com>
2024-09-16 22:43:15 +0530

    Update prime.js

--- src/JS Propblem Solving/prime.js
+++ src/JS Propblem Solving/prime.js
@@ -22,3 +22,20 @@ if(isPrime(10)){
 else{
 	console.log("its not Prime")
 }
+
+
+
+
+function prime(num){
+
+if(num<1) return false;
+if(num%2===0) || (num%3===0) return false
+
+for(let i=5;i*i<=num;i++){
+	if(num%i===0)|| (num%i+2===0) return false
+}
+	return true;
+}
+
+console.log(prime(37)); //returns true
+console.log(prime(49)); // retuns false
